ef5172d814732d3f879c981a18e1bf9ff34f8945
Amin <36745993+4Min4m@users.noreply.github.com>
2025-03-24 23:31:16 +0000

    temp allow_origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # Allow these origins
+    allow_origins=["*"],  # Allow these origins
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
