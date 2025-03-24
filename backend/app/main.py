1879936ee77d28c72f52e9b22cd3708bc67c7a90
Amin <36745993+4Min4m@users.noreply.github.com>
2025-03-24 22:59:38 +0000

    update main

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,6 @@ from .services.batch_service import process_batch
 from .utils.supabase_client import supabase
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="frontend/dist", html=True), name="static")
 # Allow specific origins
 origins = [
     "http://localhost:3000",  # React frontend (or other frontend origin)
